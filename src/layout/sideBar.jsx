3d959e26c8d0487a7f22d51545ca5a7f351c4726
Remees Salam <remeessalam@Remeess-Air.bbrouter>
2023-01-08 15:56:08 +0530

    logout button changed in mobile view

--- src/layout/sideBar.jsx
+++ src/layout/sideBar.jsx
@@ -33,10 +33,10 @@ const SideBar = (props) => {
 
     function logout() {
         localStorage.clear()
-       
+
         navigate('/login')
     }
-    return ( 
+    return (
         <>
             {/* MAIN DIV */}
             <div className='flex'>
@@ -187,8 +187,9 @@ const SideBar = (props) => {
                                 </Link>
                                 <button className="flex px-4 py-2" onClick={logout}>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10">
-                                        <path fillRule="evenodd" d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z" clipRule="evenodd" />
+                                        <path fillRule="evenodd" d="M12 2.25a.75.75 0 01.75.75v9a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM6.166 5.106a.75.75 0 010 1.06 8.25 8.25 0 1011.668 0 .75.75 0 111.06-1.06c3.808 3.807 3.808 9.98 0 13.788-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788a.75.75 0 011.06 0z" clipRule="evenodd" />
                                     </svg>
+
 
 
                                 </button>
